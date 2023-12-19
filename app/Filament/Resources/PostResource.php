645e1f194390aed019fa3d5af4e14d31af429fa2
Erik Hanson <erik@erikhanson.dev>
2023-12-19 11:46:54 -0800

    Add post with photos and tags

---
+++ app/Filament/Resources/PostResource.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PostResource\Pages;
+use App\Post\Post;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class PostResource extends Resource
+{
+    protected static ?string $model = Post::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('description')
+                    ->required(),
+                Forms\Components\DateTimePicker::make('date_taken')
+                    ->required(),
+                Forms\Components\SpatieMediaLibraryFileUpload::make('photo')
+                    ->conversion('preview')
+                    ->responsiveImages()
+                    ->required(),
+                Forms\Components\SpatieTagsInput::make('tags'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\SpatieMediaLibraryImageColumn::make('photo')
+                    ->conversion('preview')
+                    ->circular(),
+                Tables\Columns\TextColumn::make('description')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('date_taken')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\SpatieTagsColumn::make('tags'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPosts::route('/'),
+            'create' => Pages\CreatePost::route('/create'),
+            'edit' => Pages\EditPost::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Post/PostImageWidthCalculator.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Post;
+
+use Illuminate\Support\Collection;
+use Spatie\MediaLibrary\ResponsiveImages\WidthCalculator\WidthCalculator;
+use Spatie\MediaLibrary\Support\ImageFactory;
+
+class PostImageWidthCalculator implements WidthCalculator
+{
+    // TODO: Implement smaller selection of responsive images. Ideally full-size, display size, and v small
+
+    public function calculateWidthsFromFile(string $imagePath): Collection
+    {
+        $image = ImageFactory::load($imagePath);
+
+        $width = $image->getWidth();
+        $height = $image->getHeight();
+        $fileSize = filesize($imagePath);
+
+        return $this->calculateWidths($fileSize, $width, $height);
+    }
+
+    public function calculateWidths(int $fileSize, int $width, int $height): Collection
+    {
+        $targetWidths = collect();
+
+        $targetWidths->push($width);
+
+        $ratio = $height / $width;
+        $area = $height * $width;
+
+        $predictedFileSize = $fileSize;
+        $pixelPrice = $predictedFileSize / $area;
+
+        while (true) {
+            $predictedFileSize *= 0.7;
+
+            $newWidth = (int) floor(sqrt(($predictedFileSize / $pixelPrice) / $ratio));
+
+            if ($this->finishedCalculating((int) $predictedFileSize, $newWidth)) {
+                return $targetWidths;
+            }
+
+            $targetWidths->push($newWidth);
+        }
+    }
+
+    protected function finishedCalculating(int $predictedFileSize, int $newWidth): bool
+    {
+        if ($newWidth < 20) {
+            return true;
+        }
+
+        if ($predictedFileSize < (1024 * 10)) {
+            return true;
+        }
+
+        return false;
+    }
+}
