1d7b74442c8f06be547b95b77d58a0cf67a1f715
Erik Hanson <erik@erikhanson.dev>
2024-01-02 12:43:20 -0800

    Add photo card

---
+++ app/View/Components/PhotoCard.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\View\Components;
+
+use App\Post\Post;
+use Carbon\Carbon;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+use Livewire\Attributes\Locked;
+
+class PhotoCard extends Component
+{
+    #[Locked]
+    public string $displayDate;
+
+    #[Locked]
+    public string $altDate;
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        public Post $post
+    )
+    {
+        $this->altDate = $post->date_taken->format('Y-m-d, g:i a');
+        if ($post->date_taken->lt(Carbon::now()->subWeek())) {
+            $this->displayDate = $this->post->date_taken->format('M j, Y');
+        } else {
+            $this->displayDate = $post->date_taken->diffForHumans();
+        }
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.photo-card');
+    }
+}
--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -30,7 +30,7 @@
 
             <ul
                 tabIndex="0"
-                class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+                class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52 z-50"
             >
                 @auth
                     <li>
