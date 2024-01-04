a4061d6ba7982deb6dc936ec6cda2b2a1b19cdf2
Erik Hanson <erik@erikhanson.dev>
2024-01-04 14:24:14 -0800

    Use slug instead of ID for tag feed URLs

--- resources/views/components/photo-card.blade.php
+++ resources/views/components/photo-card.blade.php
@@ -17,7 +17,7 @@
         </h2>
         <div class="card-actions">
             @foreach($post->tags as $tag)
-                <a class="link link-hover" href="{{ route('tag', ['tag' => $tag->id]) }}" wire:navigate>
+                <a class="link link-hover" href="{{ route('tag', ['slug' => $tag->slug]) }}" wire:navigate>
                     #{{ $tag->name }}
                 </a>
             @endforeach
