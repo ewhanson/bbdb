e30421350f8fa3c5b024a1867e42af27501422ee
Erik Hanson <erik@erikhanson.dev>
2024-01-05 12:51:50 -0800

    Change admin colour 🤷

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -35,7 +35,7 @@ class AdminPanelProvider extends PanelProvider
                     ->url(fn (): string => route('feed')),
             ])
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Emerald,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
