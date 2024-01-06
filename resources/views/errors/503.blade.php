a36d40bf88afb0a4bc79b0860439c2a2f33183eb
Erik Hanson <erik@erikhanson.dev>
2024-01-05 21:50:47 -0800

    Add 503 page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,20 @@
+<style>
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        padding: 2rem;
+    }
+
+    h1 {
+        display: flex;
+        justify-content: center;
+        margin-bottom: 4rem;
+    }
+
+    p {
+        display: flex;
+        justify-content: center;
+    }
+</style>
+<h1>Oops... 🚧</h1>
+<p>The site is currently under maintenance and will be back soon!</p>
+<p>Love, Babygramz ❤️</p>
