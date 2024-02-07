5e43885ae65f997881c15151bb3004cbb2a64363
Erik Hanson <erik@erikhanson.dev>
2024-02-07 11:30:54 -0800

    Hide alert icon below small breakpoint

--- resources/views/livewire/pages/feed.blade.php
+++ resources/views/livewire/pages/feed.blade.php
@@ -3,9 +3,9 @@
         🤷 Oops... No photos found
     @endif
         @if($newPhotoCount > 0)
-            <a href="{{ route('new-feed') }}" role="alert" class="alert w-full  sm:max-w-md">
+            <a href="{{ route('new-feed') }}" role="alert" class="alert w-full sm:max-w-md">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                     class="stroke-info shrink-0 w-6 h-6">
+                     class="stroke-info shrink-0 w-6 h-6 hidden sm:block">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                           d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                 </svg>
