8d70b9d7472b7ed779abef7b424ae64daec8ece9
Erik Hanson <erik@erikhanson.dev>
2024-07-11 15:49:44 -0700

    Update about page

--- resources/views/livewire/pages/about.blade.php
+++ resources/views/livewire/pages/about.blade.php
@@ -3,15 +3,14 @@
         <h1>About Babygramz</h1>
         <p>
             Thanks for visiting! Babygramz is a digital scrapbook for sharing
-            photos with friends and family. It allows you to create a
+            photos with friends and family—a <a href="https://www.robinsloan.com/notes/home-cooked-app/"
+                                                target="_blank">home-cooked app</a> . It allows you to create a
             password-protected photo feed with the ethos of a physical scrapbook
-            and feel of a modern social media app. For now, the project is limited
-            to a single user/photo feed, but I'd like to expand and open-source it
-            in the future.
+            and feel of a modern social media app.
         </p>
         <p>
-            Built with️ 🤎 and <a href="https://laravel.com/">Laravel</a> and
-            <a href="https://daisyui.com/">DaisyUI</a>.
+            Built with️ 🤎 and <a href="https://laravel.com/" target="_blank">Laravel</a> and
+            <a href="https://daisyui.com/" target="_blank">DaisyUI</a>.
         </p>
     </article>
 </x-main-content-layout>
